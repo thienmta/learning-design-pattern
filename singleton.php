e98f60a08895804df8384d7b279f242b25227376
ThienMTA <thienmta1406@gmail.com>
2018-04-18 15:52:22 +0700

    change functions

--- singleton.php
+++ singleton.php
@@ -1,28 +1,33 @@
 <?php
 class WifiCompany {
-	static private $password = null;
-	public $username = null;
+	static private $checkRole = null;
+	public $password;
+	public $username;
 
 	static function getInstance () {
-		if ( self::$password == null ) {
-			self::$password = new WifiCompany();
+		if ( self::$checkRole == null ) {
+			self::$checkRole = new WifiCompany();
+			return self::$checkRole;
+		} else {
+			echo "<b>Only admin can edit!</b>";
 		}
-		return self::$password;
 	}
 
 	function setWifiInfomation ( $username, $password ) {
-		this->username = $username;
-		this->password = $password;
+		$this->username = $username;
+		$this->password = $password;
+		$this->checkRole = 1;
 	}
 
 	function getWifiInfomation () {
 		echo "Infomation wifi our company: <br>";
-		echo "Username: ".this->username."<br>";
-		echo "Password: ".this->password."<br>";
+		echo "Username: ".$this->username."<br>";
+		echo "Password: ".$this->password."<br>";
 	}
 
 	function editWifiInfomation ( $username, $password ) {
-		this->username = $username;
-		this->password = $password;
+		$this->username = $username;
+		$this->password = $password;
 	}
 }
+?>
