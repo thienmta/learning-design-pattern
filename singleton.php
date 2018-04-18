589b73a9ac38e678d206a5d1cf562debbd104ae1
ThienMTA <thienmta1406@gmail.com>
2018-04-18 14:53:23 +0700

    commit conten functions in class

--- singleton.php
+++ singleton.php
@@ -1,20 +1,28 @@
 <?php
 class WifiCompany {
 	static private $password = null;
+	public $username = null;
 
 	static function getInstance () {
-		
+		if ( self::$password == null ) {
+			self::$password = new WifiCompany();
+		}
+		return self::$password;
 	}
 
-	function setWifiInfomation () {
-
+	function setWifiInfomation ( $username, $password ) {
+		this->username = $username;
+		this->password = $password;
 	}
 
 	function getWifiInfomation () {
-		
+		echo "Infomation wifi our company: <br>";
+		echo "Username: ".this->username."<br>";
+		echo "Password: ".this->password."<br>";
 	}
 
-	function editWifiInfomation () {
-		
+	function editWifiInfomation ( $username, $password ) {
+		this->username = $username;
+		this->password = $password;
 	}
 }
