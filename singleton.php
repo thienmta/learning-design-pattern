dd92970ca3039dac1ba182547800792fbbd14cc8
ThienMTA <thienmta1406@gmail.com>
2018-04-18 14:47:25 +0700

    commit class WifiCompany and function in this class

---
+++ singleton.php
@@ -0,0 +1,20 @@
+<?php
+class WifiCompany {
+	static private $password = null;
+
+	static function getInstance () {
+		
+	}
+
+	function setWifiInfomation () {
+
+	}
+
+	function getWifiInfomation () {
+		
+	}
+
+	function editWifiInfomation () {
+		
+	}
+}
