da55c55c5a51c1d849c18dc438443e4ac212b1bf
ThienMTA <thienmta1406@gmail.com>
2018-04-26 11:30:40 +0700

    change ex_observer

--- ex_observer.php
+++ ex_observer.php
@@ -10,14 +10,23 @@ abstract class AbstractSubject {
     abstract function notify();
 }
 
-class PatternObserver extends AbstractObserver {
+class SalaryObserver extends AbstractObserver {
     public function __construct() {
     }
-    
+
     public function update(AbstractSubject $subject) {
       echo "On day: ".$subject->getOnDay()."<br>";
       echo "OT day: ".$subject->getOtDay()."<br>";
-      echo "Update Salary: ".$subject->getSalary() ."<br><br>";
+      echo "Update Salary: ".$subject->getSalary() ."<br>";
+    }
+}
+
+class SumOnDayObserver extends AbstractObserver {
+    public function __construct() {
+    }
+
+    public function update(AbstractSubject $subject) {
+      echo "Sum on day: ".($subject->getOnDay() + $subject->getOtDay())."<br><br>";
     }
 }
 
@@ -27,40 +36,39 @@ class SalarySubject extends AbstractSubject {
     private $observers = array();
     function __construct() {
     }
-    
+
     function attach(AbstractObserver $observer) {
       $this->observers[] = $observer;
     }
-    
+
     function detach(AbstractObserver $observer) {
-      //$key = array_search($observer, $this->observers);
       foreach($this->observers as $okey => $oval) {
         if ($oval == $observer) { 
           unset($this->observers[$okey]);
         }
       }
     }
-    
+
     function notify() {
       foreach($this->observers as $obs) {
         $obs->update($this);
       }
     }
-    
+
     function updateSalary($onDay, $otDay) {
       $this->onDay = $onDay;
       $this->otDay = $otDay;
       $this->notify();
     }
-    
+
     function getSalary() {
       return ( $this->onDay * 150000 + $this->otDay * 200000 );
     }
-    
+
     function getOnDay() {
       return $this->onDay;
     }
-    
+
     function getOtDay() {
       return $this->otDay;
     }
@@ -68,13 +76,17 @@ class SalarySubject extends AbstractSubject {
 
   echo "SALARY <br><br>";
 
-  $patternGossiper = new SalarySubject();
-  $patternGossipFan = new PatternObserver();
+  $subject = new SalarySubject();
+  $salary = new SalaryObserver();
+  $sumOnDay = new SumOnDayObserver();
 
-  $patternGossiper->attach($patternGossipFan);
-  $patternGossiper->updateSalary(20, 3);
-  $patternGossiper->updateSalary(18, 2);
-  $patternGossiper->updateSalary(12, 4);
+  $subject->attach($salary);
+  $subject->attach($sumOnDay);
 
-  $patternGossiper->detach($patternGossipFan);
-  $patternGossiper->updateSalary(19, 2);
+  $subject->updateSalary(20, 3);
+  $subject->updateSalary(18, 2);
+  $subject->updateSalary(12, 4);
+
+  $subject->detach($salary);
+  $subject->detach($sumOnDay);
+  $subject->updateSalary(19, 2);
